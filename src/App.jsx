d114c0485556025a3e8eaa4dbc7bd0c7c4a4a3ad
skylarshi123 <skylarshi123@gmail.com>
2024-07-28 00:52:49 -0700

    The TodoList Component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import './App.css'
+import TodoList from "./TodoList";
+
+function App() {
+
+  return (
+    <>
+      <CssBaseline />
+      <h1>To Do List</h1>
+      <TodoList />
+    </>
+  )
+}
+
+export default App
+
+
